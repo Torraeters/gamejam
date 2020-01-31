4c45cffa3e23f8a0499d24189454524c77dbc8a7
Joan Calabuig <joacaar@epsg.upv.es>
2020-01-31 23:20:11 +0100

    Clase Objeto con modificaciones de "Controles"

---
+++ Assets/_Clock/claseObjeto/Objeto.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objeto : MonoBehaviour
+{
+
+    public int angulo;
+
+    public float posicionX;
+    public float posicionY;
+
+    public bool esTornillo;
+    public bool gearConTornillo;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        angulo = 0;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/_Clock/claseObjeto/RotacionObjeto.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotacionObjeto : MonoBehaviour
+{
+    private int velocity = 10;
+    public float velocityDown = -0.2f;
+    private int grads = 45;
+
+    private Objeto objeto;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        objeto = GetComponent<Objeto>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, velocityDown * Time.deltaTime, 0, Space.World);
+        if (Input.GetButton("Horizontal"))
+        {
+            if (Input.GetAxis("Horizontal") > 0)
+            {
+                transform.Translate(velocity * Time.deltaTime, 0, 0, Space.World);
+            }
+            else
+            {
+                transform.Translate(-velocity * Time.deltaTime, 0, 0, Space.World);
+            }
+        }
+        //Giro izquierda
+        if (Input.GetButtonDown("Fire1"))
+        {
+            transform.Rotate(0, 0, grads);
+            if(objeto.angulo == 180)
+            {
+                objeto.angulo = -135;
+            }
+            else
+            {
+                objeto.angulo = objeto.angulo + grads;
+            }
+        }
+
+        // Giro Derecha
+        if (Input.GetButtonDown("Fire2"))
+        {
+            transform.Rotate(0, 0, -grads);
+
+            if (objeto.angulo == -180)
+            {
+                objeto.angulo = 135;
+            }
+            else
+            {
+                objeto.angulo = objeto.angulo - grads;
+            }
+        }
+    }
+}
