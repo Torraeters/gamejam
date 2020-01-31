51b45789c81e36db1b5ec98645b279cda492b839
joafusda <joafusda@epsg.upv.es>
2020-01-31 22:00:31 +0100

    Creado objeto CaidaObjetos

---
+++ Assets/_Clock/CaidaObjetosScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaidaObjetosScript : MonoBehaviour
+{
+
+    // Grupo de objetos
+    public GameObject[] piezas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Deja caer la primera pieza
+        dejarCaerSiguiente();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void dejarCaerSiguiente()
+    {
+        // Random Index
+        int i = Random.Range(0, piezas.Length);
+
+        // Spawn Group at current Position
+        Instantiate(piezas[i],
+                    transform.position,
+                    Quaternion.identity);
+    }
+
+}
