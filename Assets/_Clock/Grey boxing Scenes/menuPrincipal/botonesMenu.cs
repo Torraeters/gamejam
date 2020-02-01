f242465b1dde767681e1724461028ac1b6e81b0f
Joan Calabuig <joacaar@epsg.upv.es>
2020-02-01 20:11:37 +0100

    Menu principal

---
+++ Assets/_Clock/Grey boxing Scenes/menuPrincipal/botonesMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class botonesMenu : MonoBehaviour
+{
+    public Animator controladorAnim;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void botonDerecha()
+    {
+        if (!controladorAnim.GetBool("primeraTransicion"))
+        {
+            controladorAnim.SetBool("primeraTransicion", true);
+            controladorAnim.SetBool("muestraPlay", true);
+        }
+        else
+        {
+            controladorAnim.SetBool("muestraPlay", true);
+        }
+    }
+
+    public void botonIzquierda()
+    {
+        controladorAnim.SetBool("muestraPlay", false);
+    }
+}
