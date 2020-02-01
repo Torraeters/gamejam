378ede2c6e185fb55a9de9a041144401146a726f
Emilia Rosa van der Heide <emvader@epsg.upv.es>
2020-02-01 01:07:16 +0100

    contador con aguja en movimiento

--- Assets/_Clock/Grey boxing Scenes/Contador/Contador.cs
+++ Assets/_Clock/Grey boxing Scenes/Contador/Contador.cs
@@ -13,22 +13,44 @@ public class Contador : MonoBehaviour
 
     public Text text;
 
+    // Cada cuántos segundos debe girar la aguja 'un segundo' de reloj
+    public float tiempoMovimientoAguja;
+
     // Calculamos la relación segundo/ángulo para saber cuánto rotar
-    // la aguja cada segundo. Cada 15 segundos debería rotar 90º.
-    float anguloEnSegundos = 360f/60f;
+    // la aguja cada segundo. Cada 60 segundos debería rotar 360º.
+    float anguloEnSegundos = 6;
+
+    float startTime;
+
+    void Start() {
+        // Cogemos el tiempo inicial
+        startTime = Time.time;
+    }
 
     void Update() {
-        // Restamos el tiempo conforme avanza
-        tiempoRestante -= Time.deltaTime;
-
-        // Rotamos la aguja
-        aguja.transform.Rotate(0.0f, 0.0f, anguloEnSegundos, Space.Self);
-
+        // Calculamos el ángulo en grados para el giro
+        float angulo = anguloEnSegundos*tiempoRestante;
+        
         // Mostramos el tiempo
         text.text = Mathf.Round(tiempoRestante) + "s";
+
+        // Cada 'segundo' moverá la aguja un segundo de reloj
+        if (Time.time - startTime >= tiempoMovimientoAguja) {
+            // Restamos el tiempo conforme avanza
+            tiempoRestante -= 1;
+
+            // Rotamos la aguja 
+            aguja.transform.Rotate(0.0f, 0.0f, -anguloEnSegundos, Space.Self);
+
+            // Actualizamos el valor de starttime
+            startTime = Time.time;
+        }
     }
 
-    public void anyadirTiempo(float tiempoExtra) {
-        tiempoRestante += tiempoExtra;
+    public void anyadirTiempo(int tiempo) {
+        tiempoRestante += tiempo;
     }
+
+
+    
 }
