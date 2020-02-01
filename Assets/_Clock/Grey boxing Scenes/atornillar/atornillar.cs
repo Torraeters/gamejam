b684c8726875a6cbdac7295171e8a8a685aa4eac
Brian Calabuig <briancalabuig@gmail.com>
2020-02-01 11:29:56 +0100

    encajar modificado

---
+++ Assets/_Clock/Grey boxing Scenes/atornillar/atornillar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class atornillar : MonoBehaviour
+{
+
+    Encajar encajar;
+    private GameObject targetPiece;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(encajar.isFitIn == true)
+        {
+        }
+    }
+}
