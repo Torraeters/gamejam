568fc3455ee8de8a0de9995736a7b414a73b3624
Brian Calabuig <briancalabuig@gmail.com>
2020-02-01 20:06:48 +0100

    Atornillar terminado aprox (gracias rosa)

--- Assets/_Clock/Grey boxing Scenes/atornillar/atornillar.cs
+++ Assets/_Clock/Grey boxing Scenes/atornillar/atornillar.cs
@@ -4,70 +4,72 @@ using UnityEngine;
 
 public class atornillar : MonoBehaviour
 {
-
-
     public int speed = 10;
     public SpriteRenderer rend;
     public Color negro = Color.black;
     public Color rojo = Color.red;
     public Color green = Color.green;
-
-
+    bool rutinaEmpezada;
+    bool atornillado;
+    int nivelAtornillado;
+    GameObject caidaObjetos;
+    CaidaObjetosScript caidaObjetosScript;
 
     // Start is called before the first frame update
     void Start()
     {
         rend = GetComponent<SpriteRenderer>();
+        nivelAtornillado = 0;
+        caidaObjetos = GameObject.Find("CaidaObjetos");
+        caidaObjetosScript = caidaObjetos.GetComponent<CaidaObjetosScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(transform.rotation.z);
-        if(transform.rotation.z <= 1 && transform.rotation.z >= -1)
+        if (nivelAtornillado < 10)
         {
-            StartCoroutine(noAtornillado());
+            if (!rutinaEmpezada)
+            {
+                StartCoroutine(noAtornillado());
+                rutinaEmpezada = true;
+            }
 
             //Detect when the up arrow key is pressed down
             if (Input.GetKeyDown(KeyCode.UpArrow))
             {
-
-              //  transform.RotateAround(transform.position, Vector3.back, speed * Time.deltaTime);
+                //  transform.RotateAround(transform.position, Vector3.back, speed * Time.deltaTime);
                 transform.Rotate(0.0f, 0.0f, -45f, Space.Self);
-
+                nivelAtornillado++;
             }
 
             //Detect when the up arrow key is pressed down
             if (Input.GetKeyDown(KeyCode.DownArrow))
             {
-
                 transform.Rotate(0.0f, 0.0f, 45f, Space.Self);
-
+                nivelAtornillado--;
             }
         }
         else
         {
-            Debug.Log("else");
-            StartCoroutine(Atornillado());
+            atornillado = true;
+            rend.material.color = green;
+            caidaObjetosScript.dejarCaerSiguiente();
+            this.enabled = false;
         }
-
     }
 
 
 
     IEnumerator noAtornillado()
     {
-        rend.material.color = rojo;
-        yield return new WaitForSeconds(0.6f);
-        rend.material.color = negro;
-        yield return new WaitForSeconds(0.3f);
-        rend.material.color = rojo;
-        yield return new WaitForSeconds(0.3f);
-        rend.material.color = negro;
-        yield return new WaitForSeconds(0.1f);
-        rend.material.color = rojo;
-        yield return new WaitForSeconds(0.1f);
-        rend.material.color = negro;
+        while (!atornillado)
+        {
+            rend.material.color = rojo;
+            yield return new WaitForSeconds(.5f);
+            rend.material.color = negro;
+            yield return new WaitForSeconds(.5f);
+        }
     }
 
     IEnumerator Atornillado()
