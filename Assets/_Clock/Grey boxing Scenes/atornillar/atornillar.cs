24bec0516fb0d7e4f8fcf87524e14fc7db8046bc
Brian Calabuig <briancalabuig@gmail.com>
2020-02-01 18:49:06 +0100

    atornillar avanços

--- Assets/_Clock/Grey boxing Scenes/atornillar/atornillar.cs
+++ Assets/_Clock/Grey boxing Scenes/atornillar/atornillar.cs
@@ -6,44 +6,91 @@ public class atornillar : MonoBehaviour
 {
 
 
-    public int speed;
-
+    public int speed = 10;
+    public bool isAtornillado = false;
+    public SpriteRenderer rend;
+    public Color negro = Color.black;
+    public Color rojo = Color.red;
+    public Color green = Color.green;
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-
+        rend = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
 
-
-                //Detect when the up arrow key is pressed down
-                if (Input.GetKeyDown(KeyCode.UpArrow))
-                {
-                    Debug.Log("hola");
-                    transform.RotateAround(transform.position, Vector3.back, speed * Time.deltaTime);
-                    Debug.Log("adios");
-                }
+        StartCoroutine(noAtornillado());
 
 
+        //Detect when the up arrow key is pressed down
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
 
-                //Detect when the up arrow key has been released
-                if (Input.GetKeyUp(KeyCode.UpArrow))
-                    Debug.Log("Up Arrow key was released.");
+            transform.RotateAround(transform.position, Vector3.back, speed * Time.deltaTime);
+            if (transform.rotation.z >= .5)
+            {
 
-                //Detect when the up arrow key is pressed down
-                if (Input.GetKeyDown(KeyCode.DownArrow))
-                    Debug.Log("Up Arrow key was pressed.");
+                StartCoroutine(Atornillado());
+                isAtornillado = true;
 
-                //Detect when the up arrow key has been released
-                if (Input.GetKeyUp(KeyCode.DownArrow))
-                    Debug.Log("Up Arrow key was released.");
             }
-        
 
+        }
+
+
+
+
+        //Detect when the up arrow key is pressed down
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+
+            transform.RotateAround(transform.position, Vector3.forward, speed * Time.deltaTime);
+            if (transform.rotation.z >= -.5)
+            {
+
+                StartCoroutine(Atornillado());
+                isAtornillado = true;
+
+            }
+
+        }
+    }
+
+
+
+    IEnumerator noAtornillado()
+    {
+        rend.material.color = rojo;
+        yield return new WaitForSeconds(0.6f);
+        rend.material.color = negro;
+        yield return new WaitForSeconds(0.3f);
+        rend.material.color = rojo;
+        yield return new WaitForSeconds(0.3f);
+        rend.material.color = negro;
+        yield return new WaitForSeconds(0.1f);
+        rend.material.color = rojo;
+        yield return new WaitForSeconds(0.1f);
+        rend.material.color = negro;
+    }
+
+    IEnumerator Atornillado()
+    {
+        rend.material.color = green;
+        yield return new WaitForSeconds(0.6f);
+        rend.material.color = negro;
+        yield return new WaitForSeconds(0.3f);
+        rend.material.color = green;
+        yield return new WaitForSeconds(0.3f);
+        rend.material.color = negro;
+        yield return new WaitForSeconds(0.1f);
+        rend.material.color = green;
+        yield return new WaitForSeconds(0.1f);
+        rend.material.color = negro;
+    }
 }
