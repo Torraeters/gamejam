5730053caa5a7bf039e60a4f7124a95e4fc5c278
Brian Calabuig <briancalabuig@gmail.com>
2020-02-01 17:41:37 +0100

    atornillar

--- Assets/_Clock/Grey boxing Scenes/atornillar/atornillar.cs
+++ Assets/_Clock/Grey boxing Scenes/atornillar/atornillar.cs
@@ -5,37 +5,28 @@ using UnityEngine;
 public class atornillar : MonoBehaviour
 {
 
-    GameObject hole;
+
     public int speed;
-    Encajar encajar;
-    GameObject tornillo;
-    Objeto obj;
+
+
 
 
     // Start is called before the first frame update
     void Start()
     {
-        hole = GameObject.FindWithTag("hole");
-        encajar = hole.GetComponent<Encajar>();
-        tornillo = GameObject.FindWithTag("tornillo");
-        obj = tornillo.GetComponent<Objeto>();
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(encajar.targetPiece != null)
-        {
-            if (encajar.isFitIn == true)
-            {
 
 
                 //Detect when the up arrow key is pressed down
                 if (Input.GetKeyDown(KeyCode.UpArrow))
                 {
                     Debug.Log("hola");
-                    tornillo.transform.RotateAround(transform.position, Vector3.back, speed * Time.deltaTime);
+                    transform.RotateAround(transform.position, Vector3.back, speed * Time.deltaTime);
                     Debug.Log("adios");
                 }
 
@@ -53,6 +44,6 @@ public class atornillar : MonoBehaviour
                 if (Input.GetKeyUp(KeyCode.DownArrow))
                     Debug.Log("Up Arrow key was released.");
             }
-        }
-    }
+        
+
 }
