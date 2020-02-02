156450bf8fcaa32ab0fc8eb0fac9900278e7d795
Joan Calabuig <joacaar@epsg.upv.es>
2020-02-02 05:34:54 +0100

    Animaciones engranajes menu principal

--- Assets/_Clock/Grey boxing Scenes/menuPrincipal/botonesMenu.cs
+++ Assets/_Clock/Grey boxing Scenes/menuPrincipal/botonesMenu.cs
@@ -6,6 +6,11 @@ public class botonesMenu : MonoBehaviour
 {
     public Animator controladorAnim;
 
+    public Animator clara1;
+    public Animator oscura1;
+    public Animator clara2;
+    public Animator oscura2;
+
 
     // Start is called before the first frame update
     void Start()
@@ -21,6 +26,8 @@ public class botonesMenu : MonoBehaviour
 
     public void botonDerecha()
     {
+        cambioSentido();
+
         if (!controladorAnim.GetBool("primeraTransicion"))
         {
             controladorAnim.SetBool("primeraTransicion", true);
@@ -34,6 +41,28 @@ public class botonesMenu : MonoBehaviour
 
     public void botonIzquierda()
     {
+        cambioSentido();
         controladorAnim.SetBool("muestraPlay", false);
+    }
+
+    public void jugar()
+    {
+
+    }
+
+    public void salir()
+    {
+        Application.Quit();
+    }
+
+
+    private void cambioSentido()
+    {
+        clara1.SetBool("girarIzquierda", !clara1.GetBool("girarIzquierda"));
+        clara2.SetBool("girarIzquierda", !clara1.GetBool("girarIzquierda"));
+
+        oscura1.SetBool("girarDerecha", !oscura1.GetBool("girarDerecha"));
+        oscura2.SetBool("girarDerecha", !oscura2.GetBool("girarDerecha"));
+
     }
 }
